74869a28518fe6e349a39af6d20c0f55bf27296a
Subham Lohar <68697812+subhamlohar@users.noreply.github.com>
2023-06-10 12:14:55 +0530

    identity added

--- BulkyWeb/obj/Debug/net7.0/SubhamBookWeb.MvcApplicationPartsAssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net7.0/SubhamBookWeb.MvcApplicationPartsAssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("SubhamBook.DataAccess")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("SubhamBook.Models")]
 
