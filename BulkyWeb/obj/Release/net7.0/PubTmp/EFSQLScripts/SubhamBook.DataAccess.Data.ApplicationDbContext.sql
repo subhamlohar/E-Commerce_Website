c92123335a5932cb34f41da8ab72cc5d69612c47
Subham Lohar <68697812+subhamlohar@users.noreply.github.com>
2023-07-01 10:53:31 +0530

    database string

---
+++ BulkyWeb/obj/Release/net7.0/PubTmp/EFSQLScripts/SubhamBook.DataAccess.Data.ApplicationDbContext.sql
@@ -0,0 +1,358 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [AspNetRoles] (
+        [Id] nvarchar(450) NOT NULL,
+        [Name] nvarchar(256) NULL,
+        [NormalizedName] nvarchar(256) NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [Categories] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(30) NOT NULL,
+        [DisplayOrder] int NOT NULL,
+        CONSTRAINT [PK_Categories] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [Companies] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [StreetAddress] nvarchar(max) NULL,
+        [City] nvarchar(max) NULL,
+        [State] nvarchar(max) NULL,
+        [PostalCode] nvarchar(max) NULL,
+        [PhoneNumber] nvarchar(max) NULL,
+        CONSTRAINT [PK_Companies] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [AspNetRoleClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [RoleId] nvarchar(450) NOT NULL,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [Products] (
+        [Id] int NOT NULL IDENTITY,
+        [Title] nvarchar(max) NOT NULL,
+        [Description] nvarchar(max) NOT NULL,
+        [ISBN] nvarchar(max) NOT NULL,
+        [Author] nvarchar(max) NOT NULL,
+        [ListPrice] float NOT NULL,
+        [Price] float NOT NULL,
+        [Price50] float NOT NULL,
+        [Price100] float NOT NULL,
+        [CategoryId] int NOT NULL,
+        [ImageUrl] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Products_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Categories] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [AspNetUsers] (
+        [Id] nvarchar(450) NOT NULL,
+        [Discriminator] nvarchar(max) NOT NULL,
+        [Name] nvarchar(max) NULL,
+        [StreetAddress] nvarchar(max) NULL,
+        [City] nvarchar(max) NULL,
+        [State] nvarchar(max) NULL,
+        [PostalCode] nvarchar(max) NULL,
+        [CompanyId] int NULL,
+        [UserName] nvarchar(256) NULL,
+        [NormalizedUserName] nvarchar(256) NULL,
+        [Email] nvarchar(256) NULL,
+        [NormalizedEmail] nvarchar(256) NULL,
+        [EmailConfirmed] bit NOT NULL,
+        [PasswordHash] nvarchar(max) NULL,
+        [SecurityStamp] nvarchar(max) NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        [PhoneNumber] nvarchar(max) NULL,
+        [PhoneNumberConfirmed] bit NOT NULL,
+        [TwoFactorEnabled] bit NOT NULL,
+        [LockoutEnd] datetimeoffset NULL,
+        [LockoutEnabled] bit NOT NULL,
+        [AccessFailedCount] int NOT NULL,
+        CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetUsers_Companies_CompanyId] FOREIGN KEY ([CompanyId]) REFERENCES [Companies] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [AspNetUserClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [UserId] nvarchar(450) NOT NULL,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [AspNetUserLogins] (
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [ProviderKey] nvarchar(450) NOT NULL,
+        [ProviderDisplayName] nvarchar(max) NULL,
+        [UserId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+        CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [AspNetUserRoles] (
+        [UserId] nvarchar(450) NOT NULL,
+        [RoleId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+        CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [AspNetUserTokens] (
+        [UserId] nvarchar(450) NOT NULL,
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [Name] nvarchar(450) NOT NULL,
+        [Value] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+        CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [OrderHeaders] (
+        [Id] int NOT NULL IDENTITY,
+        [ApplicationUserId] nvarchar(450) NOT NULL,
+        [OrderDate] datetime2 NOT NULL,
+        [ShippingDate] datetime2 NOT NULL,
+        [OrderTotal] float NOT NULL,
+        [OrderStatus] nvarchar(max) NULL,
+        [PaymentStatus] nvarchar(max) NULL,
+        [TrackingNumber] nvarchar(max) NULL,
+        [Carrier] nvarchar(max) NULL,
+        [PaymentDate] datetime2 NOT NULL,
+        [PaymentDueDate] datetime2 NOT NULL,
+        [SessionId] nvarchar(max) NULL,
+        [PaymentIntentId] nvarchar(max) NULL,
+        [Name] nvarchar(max) NOT NULL,
+        [StreetAddress] nvarchar(max) NOT NULL,
+        [City] nvarchar(max) NOT NULL,
+        [State] nvarchar(max) NOT NULL,
+        [PostalCode] nvarchar(max) NOT NULL,
+        [PhoneNumber] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_OrderHeaders] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_OrderHeaders_AspNetUsers_ApplicationUserId] FOREIGN KEY ([ApplicationUserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [ShoppingCarts] (
+        [Id] int NOT NULL IDENTITY,
+        [ProductId] int NOT NULL,
+        [Count] int NOT NULL,
+        [ApplicationUserId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_ShoppingCarts] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_ShoppingCarts_AspNetUsers_ApplicationUserId] FOREIGN KEY ([ApplicationUserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_ShoppingCarts_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE TABLE [OrderDetails] (
+        [Id] int NOT NULL IDENTITY,
+        [OrderId] int NOT NULL,
+        [ProductId] int NOT NULL,
+        [Count] int NOT NULL,
+        [Price] float NOT NULL,
+        CONSTRAINT [PK_OrderDetails] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_OrderDetails_OrderHeaders_OrderId] FOREIGN KEY ([OrderId]) REFERENCES [OrderHeaders] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_OrderDetails_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'DisplayOrder', N'Name') AND [object_id] = OBJECT_ID(N'[Categories]'))
+        SET IDENTITY_INSERT [Categories] ON;
+    EXEC(N'INSERT INTO [Categories] ([Id], [DisplayOrder], [Name])
+    VALUES (1, 1, N''Action''),
+    (2, 2, N''SciFi''),
+    (3, 3, N''History'')');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'DisplayOrder', N'Name') AND [object_id] = OBJECT_ID(N'[Categories]'))
+        SET IDENTITY_INSERT [Categories] OFF;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'City', N'Name', N'PhoneNumber', N'PostalCode', N'State', N'StreetAddress') AND [object_id] = OBJECT_ID(N'[Companies]'))
+        SET IDENTITY_INSERT [Companies] ON;
+    EXEC(N'INSERT INTO [Companies] ([Id], [City], [Name], [PhoneNumber], [PostalCode], [State], [StreetAddress])
+    VALUES (1, N''Tech City'', N''Spark'', N''7505468688'', N''723131'', N''IL'', N''123 Tech St''),
+    (2, N''Royal City'', N''Thr Photo Phactory'', N''8967407091'', N''723132'', N''WB'', N''Kashipur'')');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'City', N'Name', N'PhoneNumber', N'PostalCode', N'State', N'StreetAddress') AND [object_id] = OBJECT_ID(N'[Companies]'))
+        SET IDENTITY_INSERT [Companies] OFF;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Author', N'CategoryId', N'Description', N'ISBN', N'ImageUrl', N'ListPrice', N'Price', N'Price100', N'Price50', N'Title') AND [object_id] = OBJECT_ID(N'[Products]'))
+        SET IDENTITY_INSERT [Products] ON;
+    EXEC(N'INSERT INTO [Products] ([Id], [Author], [CategoryId], [Description], [ISBN], [ImageUrl], [ListPrice], [Price], [Price100], [Price50], [Title])
+    VALUES (1, N''Billy Spark'', 1, CONCAT(CAST(N''Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.'' AS nvarchar(max)), nchar(13), nchar(10), nchar(13), nchar(10), N''Nunc malesuada viverra ipsum sit amet tincidunt. ''), N''SWD9999001'', N'''', 99.0E0, 90.0E0, 80.0E0, 85.0E0, N''Fortune of Time''),
+    (2, N''Nancy Hoover'', 1, CONCAT(CAST(N''Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.'' AS nvarchar(max)), nchar(13), nchar(10), nchar(13), nchar(10), N''Nunc malesuada viverra ipsum sit amet tincidunt. ''), N''CAW777777701'', N'''', 40.0E0, 30.0E0, 20.0E0, 25.0E0, N''Dark Skies''),
+    (3, N''Julian Button'', 2, CONCAT(CAST(N''Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.'' AS nvarchar(max)), nchar(13), nchar(10), nchar(13), nchar(10), N''Nunc malesuada viverra ipsum sit amet tincidunt. ''), N''RITO5555501'', N'''', 55.0E0, 50.0E0, 35.0E0, 40.0E0, N''Vanish in the Sunset''),
+    (4, N''Abby Muscles'', 3, CONCAT(CAST(N''Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.'' AS nvarchar(max)), nchar(13), nchar(10), nchar(13), nchar(10), N''Nunc malesuada viverra ipsum sit amet tincidunt. ''), N''WS3333333301'', N'''', 70.0E0, 65.0E0, 55.0E0, 60.0E0, N''Cotton Candy''),
+    (5, N''Ron Parker'', 2, CONCAT(CAST(N''Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.'' AS nvarchar(max)), nchar(13), nchar(10), nchar(13), nchar(10), N''Nunc malesuada viverra ipsum sit amet tincidunt. ''), N''SOTJ1111111101'', N'''', 30.0E0, 27.0E0, 20.0E0, 25.0E0, N''Rock in the Ocean''),
+    (6, N''Laura Phantom'', 3, CONCAT(CAST(N''Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.'' AS nvarchar(max)), nchar(13), nchar(10), nchar(13), nchar(10), N''Nunc malesuada viverra ipsum sit amet tincidunt. ''), N''FOT000000001'', N'''', 25.0E0, 23.0E0, 20.0E0, 22.0E0, N''Leaves and Wonders'')');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Author', N'CategoryId', N'Description', N'ISBN', N'ImageUrl', N'ListPrice', N'Price', N'Price100', N'Price50', N'Title') AND [object_id] = OBJECT_ID(N'[Products]'))
+        SET IDENTITY_INSERT [Products] OFF;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    EXEC(N'CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL');
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_AspNetUsers_CompanyId] ON [AspNetUsers] ([CompanyId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    EXEC(N'CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL');
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_OrderDetails_OrderId] ON [OrderDetails] ([OrderId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_OrderDetails_ProductId] ON [OrderDetails] ([ProductId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_OrderHeaders_ApplicationUserId] ON [OrderHeaders] ([ApplicationUserId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_Products_CategoryId] ON [Products] ([CategoryId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_ShoppingCarts_ApplicationUserId] ON [ShoppingCarts] ([ApplicationUserId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    CREATE INDEX [IX_ShoppingCarts_ProductId] ON [ShoppingCarts] ([ProductId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230619185221_addAllTablesToDb')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230619185221_addAllTablesToDb', N'7.0.3');
+END;
+GO
+
+COMMIT;
+GO
+
